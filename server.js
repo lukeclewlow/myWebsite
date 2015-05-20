33ff3c3ecba586b00db21812d62be5eabaf830de
Luke Clewlow <luke.clewlow@gmail.com>
2015-05-20 17:49:26 +0100

    set up express server, updated dependencies

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 9999;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (request, response) {
+	response.render('index');
+});
+
+app.listen(port, function () {
+	console.log('running on http://localhost:' + port);
+});
