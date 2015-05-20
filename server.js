37bf78cbeed7c2eec6b2d928c79651c7cdcadacc
Luke Clewlow <luke.clewlow@gmail.com>
2015-05-20 18:27:37 +0100

    added bootstrap

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 9999;
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function (request, response) {
 	response.render('index');
